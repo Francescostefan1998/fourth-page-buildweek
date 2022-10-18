756e20fb9459af16e54788bf254e450efc674c0f
Francesostefan1998 <francescostefan98@gmail.com>
2022-10-18 19:10:40 +0200

    new update
    new update

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 
+const resetcolor = function (event) {
+    document.getElementById("one").style.color = "#561E6E";
+    document.getElementById("two").style.color = "#561E6E";
+    document.getElementById("three").style.color = "#561E6E";
+    document.getElementById("four").style.color = "#561E6E";
+    document.getElementById("five").style.color = "#561E6E";
+    document.getElementById("six").style.color = "#561E6E";
+    document.getElementById("seven").style.color = "#561E6E";
+    document.getElementById("eight").style.color = "#561E6E";
+    document.getElementById("nine").style.color = "#561E6E";
+    document.getElementById("ten").style.color = "#561E6E";  
+}
 
 
 const changeColor1 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor2 = function (event) {
     document.getElementById("two").style.color = "#00FFFF";
     document.getElementById("one").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor3 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
     document.getElementById("two").style.color = "#00FFFF";
     document.getElementById("three").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor4 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
     document.getElementById("two").style.color = "#00FFFF";
     document.getElementById("three").style.color = "#00FFFF";
     document.getElementById("four").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor5 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
@@ -29,7 +37,6 @@ const changeColor5 = function (event) {
     document.getElementById("three").style.color = "#00FFFF";
     document.getElementById("four").style.color = "#00FFFF";
     document.getElementById("five").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor6 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
@@ -38,7 +45,6 @@ const changeColor6 = function (event) {
     document.getElementById("four").style.color = "#00FFFF";
     document.getElementById("five").style.color = "#00FFFF";
     document.getElementById("six").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor7 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
@@ -48,7 +54,6 @@ const changeColor7 = function (event) {
     document.getElementById("five").style.color = "#00FFFF";
     document.getElementById("six").style.color = "#00FFFF";
     document.getElementById("seven").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor8 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
@@ -59,7 +64,6 @@ const changeColor8 = function (event) {
     document.getElementById("six").style.color = "#00FFFF";
     document.getElementById("seven").style.color = "#00FFFF";
     document.getElementById("eight").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor9 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
@@ -71,7 +75,6 @@ const changeColor9 = function (event) {
     document.getElementById("seven").style.color = "#00FFFF";
     document.getElementById("eight").style.color = "#00FFFF";
     document.getElementById("nine").style.color = "#00FFFF";
-    changequestion()
 }
 const changeColor10 = function (event) {
     document.getElementById("one").style.color = "#00FFFF";
@@ -83,20 +86,24 @@ const changeColor10 = function (event) {
     document.getElementById("seven").style.color = "#00FFFF";
     document.getElementById("eight").style.color = "#00FFFF";
     document.getElementById("nine").style.color = "#00FFFF";
-    document.getElementById("ten").style.color = "#00FFFF";
-    changequestion()
+    document.getElementById("ten").style.color = "#00FFFF";  
 }
 
 
-const questions = [" From 0 to 10, how likely would you recomand EPICODE to a friend or a collegue", "How do you rate the teacher?", "How do you rate the teacher assistant?"]
+const questions = ["From 0 to 10, how likely would you recommend EPICODE to a friend or a collegue", "How do you rate the teacher?", "How do you rate the teacher assistant?"]
 
-const changequestion = function () {
-    let random = Math.floor(Math.random() * (questions.length - 1)); 
-    console.log(random);
-    let extractedQuestion = questions[random];
-    console.log(extractedQuestion);
-    document.getElementById("feedbackQuestion").innerText = extractedQuestion;
+const changequestion = function (event) {
+    const innertext = documentgetElementById("feedbackQuestion").innerText
+if (innertext === questions[0]) {
+    document.getElementById("feedbackQuestion").innerText = questions[1]
+    }if (innertext === questions[1]) {
+        document.getElementById("feedbackQuestion").innerText = questions[2]   
+    } else {
+        document.getElementById("feedbackQuestion").innerText = questions[0]   
+    }
+
   };
+
 
 window.onload = function () {
     firestar()
